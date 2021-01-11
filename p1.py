b6f7ff729b24c0dda759650e14362e82e070e300
maadhavcbe777 <77277113+maadhavcbe777@users.noreply.github.com>
2021-01-11 18:42:01 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,3 @@
+r=float(input("enter the radius of a circle"))
+a=3.14*r*r
+print("area of the circle with radius",r,"is",a)
---
+++ p2.py
@@ -0,0 +1,3 @@
+fn=input("enter the filename")
+f=fn.split(".")
+print("extension of the file is",f[-1])
